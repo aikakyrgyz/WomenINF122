1140d68cfa83580656b179e63f5a09add1694a3f
aikakyrgyz <aikamusiconly@gmail.com>
2023-03-05 15:32:03 -0800

    .

--- GUI/gui.py
+++ GUI/gui.py
@@ -3,4 +3,9 @@ class GUI:
     def __init__(self):
         pass
     # we can make this class do all the drawing for any object
-    # or we can have a draw method in each class, like tile_board
+    # or we can have a draw method in each cl.ass, like tile_board
+    def draw_board(self,board):
+        #draws gui
+
+    def draw_tile(self, tile):
+        #draws tile
